39f3bfa7f700c035c13505b0ac1d1b8d0791d403
Usman Murtaza <159697208+usmannmurtazaa@users.noreply.github.com>
2026-09-08 16:30:01 +0500

    Create instrumentation.ts

---
+++ src/instrumentation.ts
@@ -0,0 +1,19 @@
+import * as Sentry from '@sentry/nextjs';
+
+export async function register() {
+  if (process.env.NEXT_RUNTIME === 'nodejs') {
+    Sentry.init({
+      dsn: process.env.SENTRY_DSN,
+      tracesSampleRate: 1.0,
+      enabled: process.env.NODE_ENV === 'production',
+    });
+  }
+
+  if (process.env.NEXT_RUNTIME === 'edge') {
+    Sentry.init({
+      dsn: process.env.SENTRY_DSN,
+      tracesSampleRate: 1.0,
+      enabled: process.env.NODE_ENV === 'production',
+    });
+  }
+}
